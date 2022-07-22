f1febec6aef73709df6bce94d577d81e5169ef8f
Jayne Chang <XChang@camtdf.camsys.com>
2022-07-22 01:45:09 +0000

    Adding CS Link21 working branch with updates in TM1 workflow, parameters, model UECs

--- model-files/runtime/JavaOnly_runMain.cmd
+++ model-files/runtime/JavaOnly_runMain.cmd
@@ -6,14 +6,14 @@ rem ############  PARAMETERS  ############
 :: Set the path
 call CTRAMP\runtime\SetPath.bat
 
-set HOST_IP=set_by_RuntimeConfiguration.py
+set HOST_IP=10.13.1.135
 
 rem ############  JPPF DRIVER  ############
 start "JPPF Server" java -server -Xmx16m -Dlog4j.configuration=log4j-driver.properties -Djppf.config=jppf-driver.properties org.jppf.server.DriverLauncher
 
 rem ############  HH MANAGER  ############
-start "Household Manager" java -Xms20000m -Xmx20000m -Dlog4j.configuration=log4j_hh.xml com.pb.mtc.ctramp.MtcHouseholdDataManager -hostname %HOST_IP%
+start "Household Manager" java -Xms2000m -Xmx180000m -Dlog4j.configuration=log4j_hh.xml com.pb.mtc.ctramp.MtcHouseholdDataManager -hostname %HOST_IP%
 
 rem ############  Matrix MANAGER #########
-start "Matrix Manager" java -Xms14000m -Xmx14000m -Dlog4j.configuration=log4j_mtx.xml -Djava.library.path="CTRAMP/runtime" com.pb.models.ctramp.MatrixDataServer -hostname %HOST_IP%
+start "Matrix Manager" java -Xms14000m -Xmx180000m -Dlog4j.configuration=log4j_mtx.xml -Djava.library.path="CTRAMP/runtime" com.pb.models.ctramp.MatrixDataServer -hostname %HOST_IP%
 
--- model-files/runtime/SetPath.bat
+++ model-files/runtime/SetPath.bat
@@ -2,34 +2,24 @@
 :: Utility to set the path.  Used in RunModel as well as RunMain and RunNodeX. 
 
 :: The commpath
-SET COMMPATH=X:\COMMPATH
-if "%COMPUTER_PREFIX%" == "WIN-" (  SET COMMPATH=D:\COMMPATH)
-if %computername%==MODEL2-A      (  set COMMPATH=E:\Model2A-Share\COMMPATH)
-if %computername%==MODEL2-B      (  set COMMPATH=E:\Model2B-Share\COMMPATH)
-if %computername%==MODEL2-C      (  set COMMPATH=E:\Model2C-Share\COMMPATH)
-if %computername%==MODEL2-D      (  set COMMPATH=E:\Model2D-Share\COMMPATH)
+SET COMMPATH=%~dp0\..\..
 
 :: The location of the 64-bit java development kit
-set JAVA_PATH=C:\Program Files\Java\jdk1.8.0_181
+set JAVA_PATH=C:\Program Files\Java\jre1.8.0_333
 
 :: The location of the GAWK binary executable files
-set GAWK_PATH=X:\UTIL\Gawk
-if "%COMPUTER_PREFIX%" == "WIN-" (
-  set GAWK_PATH=C:\Software\Gawk
-)
+set GAWK_PATH=C:\Program Files (x86)\GnuWin32
 
 :: The location of R and R libraries
-set R_HOME=C:\Program Files\R\R-3.5.2
-set R_LIB=C:/Users/mtcpb/Documents/R/win-library/3.5
-if "%COMPUTER_PREFIX%" == "WIN-" (
-  set R_LIB=C:/Users/Administrator/Documents/R/win-library/3.5
-)
+set R_HOME=C:\Program Files\R\R-4.2.1
+set R_LIB=C:/Program Files/R/R-4.2.1/library
+
 
 :: The location of the RUNTPP executable from Citilabs
-set TPP_PATH=C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
+:: set TPP_PATH=C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
 
 :: The location of python
-set PYTHON_PATH=C:\Python27
+set PYTHON_PATH=C:\CTRAMP_PY27
 
 :: The location of the MTC.JAR file
 set RUNTIME=CTRAMP/runtime
@@ -39,4 +29,3 @@ set PATH=%RUNTIME%;%JAVA_PATH%/bin;%TPP_PATH%;%GAWK_PATH%/bin;%PYTHON_PATH%
 
 ::  Set the Java classpath (locations where Java needs to find configuration and JAR files)
 set CLASSPATH=%RUNTIME%/config;%RUNTIME%;%RUNTIME%/config/jppf-2.4/jppf-2.4-admin-ui/lib/*;%RUNTIME%/mtc.jar
-
