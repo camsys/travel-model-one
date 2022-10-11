78fb7f95d80a4a14307bd4002e33d282fe252f2a
gritzenthaler <58857835+gritzenthaler@users.noreply.github.com>
2022-10-11 22:05:55 +0000

    minor changes to packages.R and gen_files.R
    packages.R: I moved all the package loading to the end of the script so it's easier to see which packages didn't install correctly.
    
    gen_files.R: fixed error in creating sub-directories. Additional path string was needed.

--- CS_utils/R_summary_code/packages.R
+++ CS_utils/R_summary_code/packages.R
@@ -1,32 +1,36 @@
 #Included library to check if the packages are installed and loaded properly
 
 install.packages("data.table", dependencies=TRUE)
-library(data.table)
 
 install.packages("plyr")
-library(plyr)
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
   BiocManager::install("rhdf5")
-library(rhdf5)
-
+  
 install.packages("devtools")
 devtools::install_github("gregmacfarlane/omxr")
-library(omxr)
 
 install.packages("openxlsx")
-library(openxlsx)
 
 install.packages("zoo")
-library(zoo)
 
 install.packages("reshape2")
-library(reshape2)
 
 install.packages('Rcpp')
-library('Rcpp')
-
 
 install.packages("config")
+
+
+library(data.table)
+library(plyr)
+library(rhdf5)
+library(omxr)
+library(openxlsx)
+library(zoo)
+library(reshape2)
+library(Rcpp)
 library(config)
+
+
+
