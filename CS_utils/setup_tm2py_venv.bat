a238c69ab4e8b72a9645bc2967fe76a3229554d5
Vivek Yadav <vivekyadav2691@gmail.com>
2024-05-01 17:36:42 -0500

    Update setup_tm2py_venv.bat

--- CS_utils/setup_tm2py_venv.bat
+++ CS_utils/setup_tm2py_venv.bat
@@ -11,8 +11,8 @@ python -m venv %venv_path%
 call %venv_path%\Scripts\activate.bat
 python -m pip install --upgrade pip
 pip install pipwin
-pipwin install gdal
-pipwin install fiona
+::pipwin install gdal
+::pipwin install fiona
 pip install -r %tm2py_path%\requirements.txt
 copy "%emmepath%\emme.pth" %venv_path%\Lib\site-packages\emme.pth
 cd %tm2py_path%
